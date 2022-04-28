9e011cb90319e106c2c7f969920770af42119fcf
Mounzz <senmounia@hotmail.com>
2022-04-28 13:03:16 +0200

    exo javascript 4

---
+++ public/js/main.js
@@ -0,0 +1,38 @@
+// exo 1
+const loveTartine = "j'aime les tartine et j'en mange"
+console.log(loveTartine);
+// exo 2
+const par4 = " 4 par jours"
+console.log(loveTartine + par4);
+// exo 3
+let variable3 = 5
+// exo 4
+let variable4 = 35
+console.log(variable3 + variable4);
+// exo 5
+console.log(loveTartine + " " + variable3);
+
+
+// PARTIE 2
+let bjr = "Salut tout le monde!"
+console.log(bjr);
+let bye = "Au Revoir"
+console.log(bye);
+bjr = bye
+console.log(bjr)
+
+// PARTIE 3
+let number1 = 4
+let number2 = "6"
+// exo1
+console.log(number1 > number2);
+// exo2
+console.log(number1 < number2);
+// exo 3
+console.log(number1 == number2);
+// exo 4
+console.log(number1 != number2);
+// exo 5
+console.log(number1 === number2);
+// exo 6
+console.log(number1 !== number2);
